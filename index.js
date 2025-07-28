955e39459cb1c343e900dc3bb99e089aae0e5476
Deblina Mandal <mandaldeblina19@gmail.com>
2025-07-28 11:10:52 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Create a root to render the React application
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component into the root
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
